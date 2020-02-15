8cba3af89d182ffbd8268fa8d39da46118352d75
Thamsanca Ntuli <thamibn@live.co.za>
2020-02-15 22:20:01 +0200

    first commit

---
+++ database/factories/CategoriesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Categories;
+use Faker\Generator as Faker;
+
+$factory->define(Categories::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ resources/views/front.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <section>
+            <div>
+                <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+                  <p>For more information visit website</p>
+                  <b-button variant="primary" href="#">Check Ticket Status</b-button>
+                </b-jumbotron>
+              </div>
+        </section>
+
+    </div>
+@endsection
